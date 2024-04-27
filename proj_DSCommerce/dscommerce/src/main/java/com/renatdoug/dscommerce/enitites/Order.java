f814fb602c3679668ea0ea67bb858b27555a5388
Renato Douglas <renatdoug@hotmail.com>
2024-04-27 00:03:02 -0300

    Category, Product, OrdemItem, OrdemItemPK

--- proj_DSCommerce/dscommerce/src/main/java/com/renatdoug/dscommerce/enitites/Order.java
+++ proj_DSCommerce/dscommerce/src/main/java/com/renatdoug/dscommerce/enitites/Order.java
@@ -1,6 +1,9 @@
 package com.renatdoug.dscommerce.enitites;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -30,6 +34,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
 
@@ -82,6 +89,13 @@ public class Order {
         this.payment = payment;
     }
 
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.geProduct()).toList();
+    }
 
 
 }
