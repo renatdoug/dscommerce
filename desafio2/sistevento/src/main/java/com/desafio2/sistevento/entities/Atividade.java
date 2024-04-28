4c4fceca580b7ddfeb3a1e3e3e3cd877b5532c72
Renato Douglas <renatdoug@hotmail.com>
2024-04-28 00:06:53 -0300

    Configuracao do pom; criadas entidades Atividade e Categoria

---
+++ desafio2/sistevento/src/main/java/com/desafio2/sistevento/entities/Atividade.java
@@ -0,0 +1,69 @@
+package com.desafio2.sistevento.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @Column(columnDefinition="TEXT")
+    private String descricao;
+
+    private Double preco;
+
+    @ManyToOne //MAPEAMENTO DE MUITOS PARA UM
+    @JoinColumn(name = "categoria_id") // MAPEAMENTO DA CHAVE ESTRANGEIRA - respeitando o nome que está no  banco de dados
+    private Categoria categoria;
+
+    public  Atividade() {}
+    public Atividade(String nome, String descricao, Double preco){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+
+}
---
+++ desafio2/sistevento/src/main/java/com/desafio2/sistevento/entities/Categoria.java
@@ -0,0 +1,66 @@
+package com.desafio2.sistevento.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private Set<Atividade>atividade = new HashSet<>();
+
+
+    public Categoria(){}
+
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public Set<Atividade> getAtividade() {
+        return atividade;
+    }
+
+    
+
+}
