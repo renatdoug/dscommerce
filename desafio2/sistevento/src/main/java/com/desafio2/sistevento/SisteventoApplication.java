e5b64fd32088e1249c6f65f9e5dbfa602ef181d6
Renato Douglas <renatdoug@hotmail.com>
2024-04-27 23:16:53 -0300

    sistevento criado

---
+++ desafio2/sistevento/src/main/java/com/desafio2/sistevento/SisteventoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio2.sistevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisteventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisteventoApplication.class, args);
+	}
+
+}
---
+++ desafio2/sistevento/src/test/java/com/desafio2/sistevento/SisteventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio2.sistevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisteventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
