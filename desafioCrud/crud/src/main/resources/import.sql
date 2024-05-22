af790cdb1086f076ce5754614876de90fcc766be
Renato Douglas <renatdoug@hotmail.com>
2024-05-22 20:52:49 -0300

    Update seed

---
+++ desafioCrud/crud/src/main/resources/import.sql
@@ -0,0 +1,21 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Brown', '32658965368', 5400.00, '2001-07-25', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Alex Green', '12345678900', 3000.00, '1998-02-03', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Silva', '23456789012', 4500.00, '1985-11-12', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Santos', '34567890123', 6200.00, '1990-08-30', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Oliveira', '45678901234', 3800.00, '1977-03-21', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Beatriz Costa', '56789012345', 7500.00, '1995-12-15', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernando Lima', '67890123456', 2900.00, '1983-09-05', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Julia Almeida', '78901234567', 5100.00, '2000-04-18', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ricardo Pereira', '89012345678', 3300.00, '1975-06-10', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mariana Barbosa', '90123456789', 5700.00, '1992-01-25', 1);
+
+
